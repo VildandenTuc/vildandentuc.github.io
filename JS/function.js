ddb37a24782036cb8164d8505abc263429971988
VildandenTuc <123185234+VildandenTuc@users.noreply.github.com>
2023-01-30 17:30:18 -0300

    Add files via upload

---
+++ JS/function.js
@@ -0,0 +1,7 @@
+document.getElementById('btn_ingles').onclick = function(){
+    alert("Nivel de inglés alcanzado: B2");
+}
+
+document.getElementById('btn_aleman').onclick = function(){
+    alert("Nivel de alemán alcanzado: B2");
+}
