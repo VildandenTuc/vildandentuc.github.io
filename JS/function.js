ca112bdf41546424ee6552f4c8c201e9b5a6dce6
VildandenTuc <123185234+VildandenTuc@users.noreply.github.com>
2023-03-07 19:49:47 -0300

    Delete JS directory
    corrección

--- JS/function.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('btn_ingles').onclick = function(){
-    alert("Nivel de inglés alcanzado: B2");
-}
-
-document.getElementById('btn_aleman').onclick = function(){
-    alert("Nivel de alemán alcanzado: B2");
-}
